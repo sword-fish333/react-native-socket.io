cfdd2dd5a5dd22166235dfabf1deaf7efd4b1331
Ghiurca Alin <ghiurcaalin@gmail.com>
2023-09-10 12:45:15 +0100

    f

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Image, View, Button} from 'react-native';
+import {StyleSheet, Image, View, Text, Button, ScrollView} from 'react-native';
 import Echo from 'laravel-echo';
 import io from 'socket.io-client';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 // Assuming your Laravel app is hosted on 'cryptoexcellence.club'
 const host = process.env.EXPO_PUBLIC_HOST
 const port =  process.env.EXPO_PUBLIC_PORT
 const protocol = process.env.EXPO_PUBLIC_PROTOCOL
 const socket = io(`${protocol}://${host}:${port}`);
-console.log('protocol',protocol,host)
 
 export default function App() {
+    const [trading_signal,setTradingSignal]=useState(null)
     useEffect(() => {
 
         const echo = new Echo({
@@ -22,7 +22,10 @@ export default function App() {
 
         const channel = echo.channel('trading-signal');
         channel.listen('TradingSignalEvent', e => {
-            console.log('Received trading signal:', e);
+            console.log('Received trading signal:', e.trading_signal);
+            if(e.trading_signal){
+                setTradingSignal(e.trading_signal)
+            }
         });
         echo.connector.socket.on('connect', () => {
             console.log('Successfully connected.');
@@ -69,10 +72,15 @@ export default function App() {
     return (
         <View style={styles.container}>
             <Image
-                style={{width:300,height:300}}
+                style={{width:100, height:100,marginTop:70,resizeMode:'contain'}}
                 source={require('./assets/images/logo_socket_io.png')}
             />
-            <View  style={{width:'80%',marginTop:50}}>
+            {trading_signal && <ScrollView style={{maxHeight:'70%',marginTop:30}}><Text style={{
+                fontFamily: 'monospace', // This will give a more "code-like" appearance
+                fontSize: 14,}}>
+                {JSON.stringify(trading_signal, null, 2)}
+            </Text></ScrollView>}
+            <View  style={{width:'80%',marginTop:30,marginBottom:30}}>
             <Button title={'send signal'} onPress={sendSignal}/>
             </View>
           <StatusBar style="auto"/>
@@ -85,6 +93,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
     },
 });
